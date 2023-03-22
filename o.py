edecb70f6adba226ed030a03b36de22de040368a
alizada0077 <126361357+alizada0077@users.noreply.github.com>
2023-03-22 18:11:22 +0400

    Update o.py

--- o.py
+++ o.py
@@ -1,12 +1,1 @@
-import openai
-openai.api_key = "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"
-openai.organization = "org-FPcSNLlNyG3rezPYeHPLT3BlbkFJpmCjcd4hZhyrTeNFtoGr"
-def generate_response(prompt):
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": prompt}
-        ]
-    )
-    message = completion.choices[0].message.content.strip()
-    return message
+sfsdfsdfsdf
